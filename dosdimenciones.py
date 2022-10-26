211a0252f334c8709d96cb2a12b0d2ac10cc5098
IsaacEstrada159 <70659682+IsaacEstrada159@users.noreply.github.com>
2022-10-26 15:39:52 -0500

    Update dosdimenciones.py

--- dosdimenciones.py
+++ dosdimenciones.py
@@ -1,10 +1,11 @@
 from random import random, randint
 from math import sqrt
+import matplotlib.pyplot as plt
 dim = 2
-largo = 5000
+largo = 50
 pos = [0]*dim
-
-replicas = 5
+lista = []
+replicas = 50
 def paso(pos, dim):
     d = randint(0, dim-1)
     pos[d] += -1 if random() < 0.5 else 1
@@ -15,5 +16,7 @@ for i in range(replicas):
         pos = paso(pos, dim)
         #print(pos)
     resultado = sqrt((posinicial[0] - pos[0])**2 + (posinicial[1] - pos[1])**2) 
-
-    print(resultado)
+    lista.append(resultado)
+print(lista)
+plt.boxplot(lista)
+plt.show()
