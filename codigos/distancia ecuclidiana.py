503e1c308d577aedca0da067a06581389a7b72cf
IsaacEstrada159 <70659682+IsaacEstrada159@users.noreply.github.com>
2022-11-01 21:44:37 +0100

    Create distancia ecuclidiana.py

---
+++ codigos/distancia ecuclidiana.py
@@ -0,0 +1,9 @@
+
+# Ejemplo de distancia euclidiana
+from math import sqrt
+persona = [12, 28]
+trabajo = [15, 38]
+
+resultado = sqrt((persona[0] - trabajo[0])**2 + (persona[1] - trabajo[1])**2) 
+
+print(resultado)
